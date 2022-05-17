b14094defb970bdf6321ab9f680e1f94509d1af5
Andre Claßen <andreclassen1337@gmail.com>
2022-05-17 23:39:07 +0200

    Update Reorderable.kt

--- reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/Reorderable.kt
+++ reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/Reorderable.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastFirstOrNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
@@ -67,6 +68,8 @@ class ReorderableState(val listState: LazyListState) {
     internal var movedDist by mutableStateOf(0f)
 }
 
+
+@OptIn(ExperimentalCoroutinesApi::class)
 fun Modifier.reorderable(
     state: ReorderableState,
     onMove: (fromPos: ItemPosition, toPos: ItemPosition) -> (Unit),
