e606028cd1f09ca149ac70179dcabe74bd5e4de4
Andre Claßen <andreclassen1337@gmail.com>
2022-10-27 00:59:25 +0200

    Update dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 dependencies {
     implementation(project(":reorderable"))
-    implementation("androidx.compose.runtime:runtime:1.2.1")
+    implementation("androidx.compose.runtime:runtime:1.3.0")
     implementation("androidx.compose.material:material:1.3.0")
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.navigation:navigation-compose:2.5.2")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("io.coil-kt:coil-compose:2.2.2")
 }
 
