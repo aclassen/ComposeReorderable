bb79e0bd86d1e58532d87c37b4f5b11a2e463ddd
Jan Veselý <jan.vesely92@gmail.com>
2023-04-24 20:49:33 +0200

    Compose 1.4, AGP 8.0, refactored forEachGesture with awaitEachGesture, refactored onDragStart detection

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 dependencies {
     implementation(project(":reorderable"))
-    implementation("androidx.compose.runtime:runtime:1.3.3")
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.runtime:runtime:1.4.2")
+    implementation("androidx.compose.material:material:1.4.2")
+    implementation("androidx.activity:activity-compose:1.7.1")
     implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("io.coil-kt:coil-compose:2.2.2")
 }
@@ -38,7 +38,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     namespace = "org.burnoutcrew.android"
 }
--- reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/Reorderable.kt
+++ reorderable/src/commonMain/kotlin/org/burnoutcrew/reorderable/Reorderable.kt
@@ -15,69 +15,60 @@
  */
 package org.burnoutcrew.reorderable
 
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.drag
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.AwaitPointerEventScope
 import androidx.compose.ui.input.pointer.PointerId
 import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
-import androidx.compose.ui.util.fastFirstOrNull
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 
 fun Modifier.reorderable(
     state: ReorderableState<*>
 ) = then(
-    Modifier.pointerInput(Unit) {
-        forEachGesture {
-            val dragStart = state.interactions.receive()
-            val down = awaitPointerEventScope {
-                currentEvent.changes.fastFirstOrNull { it.id == dragStart.id }
-            }
-            if (down != null && state.onDragStart(down.position.x.toInt(), down.position.y.toInt())) {
-                dragStart.offset?.apply {
-                    state.onDrag(x.toInt(), y.toInt())
-                }
-                detectDrag(
-                    down.id,
-                    onDragEnd = {
-                        state.onDragCanceled()
-                    },
-                    onDragCancel = {
-                        state.onDragCanceled()
-                    },
-                    onDrag = { change, dragAmount ->
-                        change.consume()
-                        state.onDrag(dragAmount.x.toInt(), dragAmount.y.toInt())
-                    })
-            }
-        }
-    })
+    Modifier.onGloballyPositioned { state.layoutWindowPosition.value = it.positionInWindow()}.pointerInput(Unit) {
+        awaitEachGesture {
+            val down = awaitFirstDown(requireUnconsumed = false)
 
-internal suspend fun PointerInputScope.detectDrag(
+            detectDrag(
+                down.id,
+                onDragEnd = state::onDragCanceled,
+                onDragCancel = state::onDragCanceled,
+                onDrag = { event, amount ->
+                    if (state.draggingItemIndex != null){
+                        state.onDrag(amount.x.toInt(), amount.y.toInt())
+                        event.consume()
+                    }
+                }
+            )
+
+        }
+    }
+)
+
+internal suspend fun AwaitPointerEventScope.detectDrag(
     down: PointerId,
     onDragEnd: () -> Unit = { },
     onDragCancel: () -> Unit = { },
     onDrag: (change: PointerInputChange, dragAmount: Offset) -> Unit,
 ) {
-    awaitPointerEventScope {
-        if (
-            drag(down) {
-                onDrag(it, it.positionChange())
-                it.consume()
-            }
-        ) {
-            // consume up if we quit drag gracefully with the up
-            currentEvent.changes.forEach {
-                if (it.changedToUp()) it.consume()
-            }
-            onDragEnd()
-        } else {
-            onDragCancel()
+    if (
+        drag(down) {
+            onDrag(it, it.positionChange())
         }
+    ) {
+        // consume up if we quit drag gracefully with the up
+        currentEvent.changes.forEach {
+            if (it.changedToUp()) it.consume()
+        }
+        onDragEnd()
+    } else {
+        onDragCancel()
     }
 }
-
-internal data class StartDrag(val id: PointerId, val offset: Offset? = null)
